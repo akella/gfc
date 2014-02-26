4e252b3f6a68f8c6e1f58b28f74d9f62acb3885a
Splintex <eugene.godin@gmail.com>
2014-02-26 15:56:16 +0200

    check all

--- js/common.js
+++ js/common.js
@@ -623,7 +623,31 @@ $(document).ready(function() {
   }
   tab();
 
-  
+  $(".js-check-all input").on("change", function(){
+  		var group_id = $(this).parent().attr("data-group-id");
+
+  		if ($(this).is(":checked")) {
+			//alert("checked");
+			$(".customcheckbox").each(function(){
+	  			var check_id = $(this).attr("data-id");
+	  			if (check_id == group_id) {
+	  				$(this).find("input").attr("checked","checked");
+	  				$(this).addClass("is-checked");
+	  			}
+	  		});
+		}
+		else {
+			//alert("unchecked");
+			$(".customcheckbox").each(function(){
+	  			var check_id = $(this).attr("data-id");
+	  			if (check_id == group_id) {
+	  				$(this).find("input").removeAttr("checked");
+	  				$(this).removeClass("is-checked");
+	  			}
+	  		});
+		}
+
+  });
 
 });
 
